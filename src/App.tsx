0c8cd70a26d79ac9a678f756d083f7ac81aa90d0
youssefsoua <youssefsoua94@gmail.com>
2023-08-18 09:41:54 +0100

    styles adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -33,19 +33,19 @@ const ScoreItemConfig: Record<
 > = {
   Reaction: {
     color: "text-light-red",
-    bgColor: "bg-light-red/10",
+    bgColor: "bg-light-red/5",
   },
   Memory: {
     color: "text-orangey-yellow",
-    bgColor: "bg-orangey-yellow/10",
+    bgColor: "bg-orangey-yellow/5",
   },
   Verbal: {
     color: "text-green-teal",
-    bgColor: "bg-green-teal/10",
+    bgColor: "bg-green-teal/5",
   },
   Visual: {
     color: "text-cobalt-blue",
-    bgColor: "bg-cobalt-blue/10",
+    bgColor: "bg-cobalt-blue/5",
   },
 };
 
@@ -80,7 +80,7 @@ const ScoreItem = (props: IScoreItemProps) => {
 const App = () => {
   return (
     <article className="flex w-full flex-col shadow-2xl md:h-[512px] md:w-[736px] md:flex-row md:rounded-3xl">
-      <section className="flex flex-1 flex-col items-center rounded-b-3xl bg-gradient-to-b from-light-slate-blue to-light-royal-blue p-12 text-center md:rounded-3xl">
+      <section className="flex flex-1 flex-col items-center rounded-b-3xl bg-gradient-to-b from-light-slate-blue to-light-royal-blue px-12 py-8 text-center md:rounded-3xl">
         <h1 className="mb-10 text-2xl font-bold text-light-lavender">
           Your Result
         </h1>
@@ -88,19 +88,21 @@ const App = () => {
           <span className="mb-1 text-6xl font-bold text-white">76</span>
           <p className="text-base font-medium text-light-lavender">of 100</p>
         </div>
-        <p className="mb-5 mt-10 text-2xl font-bold text-white">Great</p>
-        <p className="text-base font-medium leading-tight text-light-lavender md:max-w-[230px]">
+        <p className="mb-4 mt-8 text-2xl font-bold text-white md:text-3xl">
+          Great
+        </p>
+        <p className="text-base font-medium leading-tight text-light-lavender md:max-w-[260px] md:text-lg">
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </section>
-      <section className="flex flex-1 flex-col p-12">
+      <section className="flex flex-1 flex-col px-12 py-8">
         <h2 className="mb-8 text-2xl font-extrabold text-dark-gray-blue">
           Summary
         </h2>
         {data.map((item) => (
           <ScoreItem key={item.category} {...item} />
         ))}
-        <button className=" mt-8 rounded-full bg-dark-gray-blue py-4 text-center text-sm text-white hover:bg-violet-blue">
+        <button className=" mt-8 rounded-full bg-dark-gray-blue py-4 text-center text-sm text-white hover:bg-violet-blue md:text-base">
           Continue
         </button>
       </section>
